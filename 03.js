6d2d55a86058ac06cfc458dc344cf8021e483ba6
MemoRodz <gmo.rodriguez@gmail.com>
2022-11-03 21:17:38 -0600

    Update proposed exercises 03

--- 03.js
+++ 03.js
@@ -14,7 +14,16 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+  var respuesta = null;
+  for(var i = 0; i < amigos.length; i++)
+  {
+    if(amigos[i].nombre === nombre)
+    {
+      respuesta = amigos[i];
+    }
+  }
   
+  return respuesta;
 }
 
 // No modifiques nada debajo de esta linea //
